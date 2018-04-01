60de741019c3f16cf0d1f984b896b37840d099af
Jakub Senko <jakubsenko@gmail.com>
2018-04-01 11:23:15 +0200

    Replace headers

--- acp/akismet_module.php
+++ acp/akismet_module.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * Akismet settings ACP module.
  *
- * @package phpBB Extension - Gothick Akismet
- * @copyright (c) 2015 Matt Gibson Creative Ltd.
- * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ * Akismet. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2018 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
  *
  */
+
 namespace phpbb\akismet\acp;
 
 /**
--- tests/mock/akismet_client_check_result_mock.php
+++ tests/mock/akismet_client_check_result_mock.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ *
+ * Akismet. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2018 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
 namespace phpbb\akismet\tests\mock;
 
 class akismet_client_check_result_mock extends \Gothick\AkismetClient\Result\CommentCheckResult
